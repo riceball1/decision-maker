778dd1ee98ea723a50179e211f863d2993d48553
Dana Ng <ngdana1@gmail.com>
2021-05-24 22:49:20 -0700

    Add style updates

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,12 @@ export default {
 </script>
 
 <style>
-#app {
+#app, html, body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
-  width: 100vh;
+  background-color: #2c3e50;
+  color: white;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,20 +3,26 @@
     <h1>{{ msg }}</h1>
     <p class="description">Need to decide between two things? Use this simple app to make the decision for you.</p>
     <div class="box">
-    <input v-model="choices[0]" id="0" placeholder="Type something here..." class="choiceInput">    
-    <input v-model="choices[1]" id="1" placeholder="Type something here..." class="choiceInput">
-</div>
+      <div class="choiceInputBox">
+        <label for="choiceOne">Choice One</label>
+      <input v-model="choices[0]" id="choiceOne" placeholder="Type something here..." class="choiceInput">    
+      </div>
+      <div class="choiceInputBox">
+        <label for="choiceTwo">Choice Two</label>
+        <input v-model="choices[1]" id="choiceTwo" placeholder="Type something here..." class="choiceInput">
+      </div>
+  </div>
 <div class="box">
-<div v-bind:class="{choiceOne : 'choice'}" class="card">
-  <p>Choice 1:
-    </p> 
+<div v-bind:class="{'activeChoice' : choiceOne}" class="card">
+  <h3>One
+    </h3> 
     <p>{{ choices[0] }}</p></div>
-<div v-bind:class="{choiceTwo : 'choice'}" class="card"><p>Choice 2:</p> 
+<div v-bind:class="{'activeChoice' : choiceTwo}" class="card"><h3>Two</h3> 
 <p>{{ choices[1] }}</p></div>
 </div>
-<button v-on:click="makeDecision()" disabled="choices.length !== 2" >Decide What To Do For Me</button>
-<br/>
-<button @click="resetDecisionMaker()">Reset Decison Maker</button>
+  <button class="decisionButton" v-on:click="makeDecision()" >Decide For Me</button>
+  <br/>
+  <button class="resetButton" @click="resetDecisionMaker()">Reset Decisons</button>
     </div>
 </template>
 
@@ -29,13 +35,17 @@ export default {
   data() {
     return {
       choices: [],
-      choiceOne: false ,
+      choiceOne: false,
       choiceTwo: false,
     }
   },
   methods: {
     makeDecision: () => {
       console.log('Decide for me')
+      if (this.choices.length !== 2) {
+        alert('Please type in two choices :-) ');
+        return;
+      }
       const randomNumber = Math.floor(Math.random() * 101); 
       if (randomNumber % 2 === 0) {
         this.choiceOne = true;
@@ -53,66 +63,76 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 .description {
-  font-size: 0.8rem;
-  width: 50%;
-  margin: 0 auto;
+  font-size: 1rem;
+  width: 80%;
+  margin: 5px auto;
 }
 .main {
  margin: 0 auto;
- width: 80%;
+ padding: 10px;
 }
 .box {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+.choiceInputBox {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+}
+label {
+  font-weight: bold;
+}
 .card {
   width: 100%;
-  max-width: 300px;
+  max-width: 100%;
   height: 150px;
   border: 3px solid lightblue;
   margin: 15px 5px 5px 0;
   border-radius: 6px;
+  font-size: 1.5rem;
 }
-.choice {
+.activeChoice {
   background-color: teal;
   border: 3px dashed black;
+  color: white;
 }
 .choiceInput {
   margin: 5px;
-  width: 50%;
   height: 25px;
+  width: 100%;
   border-radius: 6px;
   border: 1px solid lightblue;
   padding: 5px;
+  font-size: 1rem;
+  background-color: lightgray;
+}
+button {
+  width: 50%;
+  height: 50px;
+  margin: 0 auto;
+  margin-top: 10px;
+  border-radius: 6px;
+  font-size: 1rem;
+}
+.decisionButton {
+  background-color: teal;
+  color: white;
+}
+.resetButton {
+  background-color: pink;
 }
 @media only screen and (max-width: 600px) {
   .box {
     flex-direction: column;
   }
-  button {
-    width: 50%;
-    height: 50px;
-    margin: 0 auto;
-    margin-top: 10px;
-  }
   .choiceInput {
-    width: 50%;
+    width: 100%;
     height: 50px;
     margin: 0 auto;
   }

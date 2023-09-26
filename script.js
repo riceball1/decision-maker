1186d28fabbffc4ef17d9218631fec416e67975d
Dana Ng <ngdana1@gmail.com>
2023-09-26 00:28:06 -0700

    update style

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const darkText = 'dark';
 
 // Set the initial theme based on local storage or default to light theme
 if (currentTheme) {
-    themeToggleSpan.innerHTML = currentTheme === 'dark-theme'? lightText : darkText;
+    themeToggleSpan.innerHTML = currentTheme === 'dark-theme' ? lightText : darkText;
     body.classList.add(currentTheme);
 } else {
     themeToggleSpan.innerHTML = lightText;
@@ -32,14 +32,35 @@ themeToggle.addEventListener('click', () => {
 
 const decisionButton = document.querySelector('.decision-button');
 const chosenOption = document.querySelector('.chosen-option');
-
+const decisionDisplay = document.querySelector('.decision-display');
+console.log(decisionDisplay)
 decisionButton.addEventListener('click', () => {
     const choice1 = document.querySelector('input[placeholder="Choice 1"]').value;
     const choice2 = document.querySelector('input[placeholder="Choice 2"]').value;
 
     // Randomly select one of the choices
     const randomChoice = Math.random() < 0.5 ? choice1 : choice2;
+    console.log(randomChoice)
+    if(randomChoice.length > 0) {
+        chosenOption.classList.add('show')
+        decisionDisplay.style.display = 'block';    
+        // Display the chosen option
+        chosenOption.textContent = randomChoice;
+    }
 
-    // Display the chosen option
-    chosenOption.textContent = randomChoice;
+});
+
+
+const resetButton = document.querySelector('.reset-button');
+
+resetButton.addEventListener('click', () => {
+    const choice1 = document.querySelector('input[placeholder="Choice 1"]')
+    choice1.innerHTML = "";
+    choice1.value = "";
+    const choice2 = document.querySelector('input[placeholder="Choice 2"]')
+    choice2.innerHTML = "";
+    choice2.value = "";
+    document.querySelector('.chosen-option').innerHTML = "";
+    decisionDisplay.style.display = 'none';
+    chosenOption.classList.remove('show')
 });

9d60886d8160be92615163e83685db5814993ffb
Dana Ng <ngdana1@gmail.com>
2023-09-25 18:52:26 -0700

    make basic

---
+++ script.js
@@ -0,0 +1,45 @@
+const themeToggle = document.getElementById('theme-toggle');
+const themeToggleSpan = document.getElementById('theme-toggle-front');
+const body = document.body;
+const currentTheme = localStorage.getItem('theme');
+const lightText = 'light';
+const darkText = 'dark';
+
+// Set the initial theme based on local storage or default to light theme
+if (currentTheme) {
+    themeToggleSpan.innerHTML = currentTheme === 'dark-theme'? lightText : darkText;
+    body.classList.add(currentTheme);
+} else {
+    themeToggleSpan.innerHTML = lightText;
+    body.classList.add('light-theme');
+}
+
+themeToggle.addEventListener('click', () => {
+    if (body.classList.contains('light-theme')) {
+        themeToggleSpan.innerHTML = lightText;
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark-theme');
+    } else {
+        themeToggleSpan.innerHTML = darkText;
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        localStorage.setItem('theme', 'light-theme');
+    }
+    console.log('themeToggleSpan', themeToggleSpan)
+});
+
+
+const decisionButton = document.querySelector('.decision-button');
+const chosenOption = document.querySelector('.chosen-option');
+
+decisionButton.addEventListener('click', () => {
+    const choice1 = document.querySelector('input[placeholder="Choice 1"]').value;
+    const choice2 = document.querySelector('input[placeholder="Choice 2"]').value;
+
+    // Randomly select one of the choices
+    const randomChoice = Math.random() < 0.5 ? choice1 : choice2;
+
+    // Display the chosen option
+    chosenOption.textContent = randomChoice;
+});
--- src/lib/utils/Types.ts
+++
@@ -1,4 +0,0 @@
-export type Message = {
-	user: string;
-	text: string;
-};
